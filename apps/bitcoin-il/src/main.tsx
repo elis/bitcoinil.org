e2bcbc62cc2081802a5b8a9c4d2fd12fdc5856a8
Rick <rickbgove@gmail.com>
2022-06-29 15:15:04 +0200

    Chagne to browser router to see if fixes themes

--- apps/bitcoin-il/src/main.tsx
+++ apps/bitcoin-il/src/main.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 
 ReactDOM.render(
