b3bc1c2c6f6785dcfa86e8a2bd4a432fef3112ca
Rick <rickbgove@gmail.com>
2022-06-30 10:18:24 +0200

    Clean up header styling declarations

--- apps/bitcoin-il/src/Header.tsx
+++ apps/bitcoin-il/src/Header.tsx
@@ -1,25 +1,19 @@
 import * as React from 'react'
-import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
+
 import { phoneDevices } from './breakpoints'
 import BurgerMenu from './BurgerMenu'
+import CustomNavLink from './CustomNavLink'
 import HeaderMenu from './HeaderMenu'
+import logo from './img/logo.svg'
 import { HeaderProps } from './Interfaces'
 import LanguageSelect from './LanguageSelect'
-import logo from './img/logo.svg'
 import ThemeSwitch from './ThemeSwitch'
-import { useRecoilValue } from 'recoil'
-import { currentlySelectedLanguage } from './state/state'
-import { useIntl } from './hooks/useIntl'
-import CustomNavLink from './CustomNavLink'
 
 const Header: React.FC<HeaderProps> = ({ setLanguage }) => {
-  const atomLang = useRecoilValue(currentlySelectedLanguage)
-  const navigate = useNavigate()
-  const { customNavigate } = useIntl()
   return (
     <StyledHeader id="Header">
-      <div className="left">
+      <div className="header-left">
         <CustomNavLink to="/">
           <div className="logo">
             <img src={logo} />
@@ -46,34 +40,39 @@ const StyledHeader = styled.div`
 
   display: flex;
   height: 60px;
-  padding: 0 70px;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
 
-  .logo {
-    margin: 0;
-    cursor: pointer;
-    transition: all 200ms;
-    &:hover {
-      opacity: 0.5;
-      transition: all 200ms;
+  .header {
+    &-left {
+      .logo {
+        margin: 0;
+        cursor: pointer;
+        transition: all 200ms;
+        &:hover {
+          opacity: 0.5;
+          transition: all 200ms;
+        }
+
+        img {
+          height: 20px;
+        }
+      }
     }
 
-    img {
-      height: 20px;
+    &-middle {
+      ${phoneDevices} {
+        .header-middle {
+          display: none;
+        }
+      }
     }
-  }
 
-  ${phoneDevices} {
-    .header-middle {
-      display: none;
+    &-right {
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
-  }
-
-  .header-right {
-    display: flex;
-    align-items: center;
-    justify-content: center;
   }
 `
 
