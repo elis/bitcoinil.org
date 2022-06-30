21bdb9a7cced5a67e6530f5334bf0982cbae1fe4
Rick <rickbgove@gmail.com>
2022-06-30 14:43:41 +0200

    language select mobile works

--- apps/bitcoin-il/src/LanguageSelectMobile.tsx
+++ apps/bitcoin-il/src/LanguageSelectMobile.tsx
@@ -1,13 +1,65 @@
+import { Menu } from 'antd'
 import * as React from 'react'
-import { FormattedMessage } from 'react-intl'
+import { useLocation } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
 import styled from 'styled-components'
+import { useIntl } from './hooks/useIntl'
+import { currentlySelectedLanguage, isBurgerMenuOpen } from './state/state'
 
-export interface LanguageSelectMobileProps {}
+export interface LanguageSelectMobileProps {
+  setLanguage: Function
+}
 
-const LanguageSelectMobile: React.FC<LanguageSelectMobileProps> = ({}) => {
+const LanguageSelectMobile: React.FC<LanguageSelectMobileProps> = ({
+  setLanguage
+}) => {
+  const [, setHideBurgerMenu] = useRecoilState(isBurgerMenuOpen)
+  const [menuOpen, setMenuOpen] = React.useState(false)
+  const { availableLanguages, customNavigate } = useIntl()
+  const [current, setCurrent] = React.useState('English')
+  const [openKeys, setOpenKeys] = React.useState([])
+
+  const onOpenChange = (keys: any) => {
+    setOpenKeys(keys)
+  }
+
+  const [, setAtomLang] = useRecoilState(currentlySelectedLanguage)
+  const location = useLocation()
+
+  const onClick = (e: any) => {
+    setLanguage(e.key)
+    setCurrent(e.key)
+    setAtomLang({ language: e.key })
+    customNavigate(location.pathname, e.key)
+    setMenuOpen(false)
+    setHideBurgerMenu(false)
+  }
+
   return (
     <StyledLanguageSelectMobile id="LanguageSelectMobile">
-      <p>LanguageSelectMobile</p>
+      <Menu
+        mode="inline"
+        selectedKeys={[current]}
+        openKeys={openKeys}
+        onClick={() => {
+          setOpenKeys([])
+          setMenuOpen(false)
+        }}
+        onOpenChange={onOpenChange}
+      >
+        <Menu.SubMenu
+          key={'lang-submenu'}
+          title={<p className="collapsable-menu">Select Language</p>}
+        >
+          {availableLanguages.map((avLang, i) => {
+            return (
+              <Menu.Item onClick={onClick} key={`${avLang.name}`}>
+                {avLang.longName}
+              </Menu.Item>
+            )
+          })}
+        </Menu.SubMenu>
+      </Menu>
     </StyledLanguageSelectMobile>
   )
 }
