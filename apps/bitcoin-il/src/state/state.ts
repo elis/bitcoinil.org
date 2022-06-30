57c5d269997552fc8b33b51010df2eec7ce889b4
Rick <rickbgove@gmail.com>
2022-06-30 14:44:11 +0200

    add is burger menu open to recoil state var

--- apps/bitcoin-il/src/state/state.ts
+++ apps/bitcoin-il/src/state/state.ts
@@ -8,3 +8,8 @@ export const currentlySelectedLanguage = atom<LanguageAtomValue>({
   key: 'currentlySelectedLanguage',
   default: { language: 'en' }
 })
+
+export const isBurgerMenuOpen = atom<boolean>({
+  key: 'isBurgerMenuOpen',
+  default: false
+})
