31c1aa4bf7cdd52f21c86a09151e2cb779229ee9
Rick <rickbgove@gmail.com>
2022-07-01 12:22:22 +0200

    Fix getting started error

--- apps/bitcoin-il/src/GettingStartedBody.tsx
+++ apps/bitcoin-il/src/GettingStartedBody.tsx
@@ -9,7 +9,7 @@ import { Divider } from 'antd'
 
 const howToUseTimelineItems: JSX.Element[] = [
   <TimelineItem
-    key="inform-yourself"
+    // key="inform-yourself"
     title="Inform yourself"
     body="Bitcoin is different than what you know and use every day. Before you start using Bitcoin, there are a few things that you need to know in order to use it securely and avoid common pitfalls."
     buttonText="Read More"
@@ -18,7 +18,7 @@ const howToUseTimelineItems: JSX.Element[] = [
     }}
   />,
   <TimelineItem
-    key="choose-wallet"
+    // key="choose-wallet"
     title="Choose your wallet"
     body="Free bitcoin wallets are available for all major operating systems and devices to serve a variety of your needs. For example, you can install an app on your mobile device for everyday use or you can have a wallet only for online payments on your computer. In any case, choosing a wallet is easy and can be done in minutes."
     buttonText="Choose your wallet"
@@ -27,7 +27,7 @@ const howToUseTimelineItems: JSX.Element[] = [
     }}
   />,
   <TimelineItem
-    key="get-btc"
+    // key="get-btc"
     title="Get BitCoin Il"
     body="You can get Bitcoin by accepting it as a payment for goods and services. There are also several ways you can buy Bitcoin."
     buttonText="Buy Bitcoin Il"
@@ -36,7 +36,7 @@ const howToUseTimelineItems: JSX.Element[] = [
     }}
   />,
   <TimelineItem
-    key="spend-bitcoin"
+    // key="spend-bitcoin"
     title="Spend Bitcoin"
     body="There are a growing number of services and merchants accepting Bitcoin all over the world. Use Bitcoin to pay them and rate your experience to help them gain more visibility."
     buttonText="Read More"
@@ -48,7 +48,7 @@ const howToUseTimelineItems: JSX.Element[] = [
 
 const howToAcceptTimelineItems: JSX.Element[] = [
   <TimelineItem
-    key="inform-yourself"
+    // key="inform-yourself"
     title="Inform yourself"
     body="Bitcoin does not require merchants to change their habits. However, Bitcoin is different than what you know and use every day. Before you start using Bitcoin, there are a few things that you need to know in order to use it securely and avoid common pitfalls."
     buttonText="Read More"
@@ -57,7 +57,7 @@ const howToAcceptTimelineItems: JSX.Element[] = [
     }}
   />,
   <TimelineItem
-    key="processing-payments"
+    // key="processing-payments"
     title="Processing payments"
     body="You can process payments and invoices by yourself or you can use merchant services and deposit money in your local currency or bitcoins. Most point of sales businesses use a tablet or a mobile phone to let customers pay with their mobile phones."
     buttonText="Find merchant services"
@@ -66,7 +66,7 @@ const howToAcceptTimelineItems: JSX.Element[] = [
     }}
   />,
   <TimelineItem
-    key="accounting-and-taxes"
+    // key="accounting-and-taxes"
     title="Accounting and Taxes"
     body="Merchants often deposit and display prices in their local currency. In other cases, Bitcoin works similarly to a foreign currency. To get appropriate guidance regarding tax compliance for your own jurisdiction, you should contact a qualified accountant."
     buttonText="Read More"
@@ -75,7 +75,7 @@ const howToAcceptTimelineItems: JSX.Element[] = [
     }}
   />,
   <TimelineItem
-    key="gaining-visibility"
+    // key="gaining-visibility"
     title="Gaining Visibility"
     body="There is a growing number of users searching for ways to spend their bitcoins. You can submit your business in online directories to help them easily find you. You can also display the Bitcoin logo on your website or your brick and mortar business."
     buttonText="Submit Your Business"
--- apps/bitcoin-il/src/Interfaces.ts
+++ apps/bitcoin-il/src/Interfaces.ts
@@ -181,7 +181,6 @@ export interface TimelineCompProps {
 export interface GettingStartedBodyProps {}
 
 export interface TimelineItemProps {
-  key: string
   title: string
   body: string
   buttonText: string
